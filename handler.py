17868d23a01d3fa4e32f6390e5fcadb209fc4bc8
Matvey Semenov <matveytri777@gmail.com>
2021-10-03 16:54:00 +0300

    delete random proxy

--- handler.py
+++ handler.py
@@ -1,11 +1,11 @@
 import sys
-import random
 import threading
 import time
 import requests
 
 from datetime import datetime, timedelta
 
+from settings import PROXY
 from product import Product
 
 
@@ -20,42 +20,29 @@ def headers_is_right(headers: dict) -> None:
         print('Check please: COOKIE, CSRFTOKEN, headers')
         sys.exit(1)
 
-def get_random_proxy() -> str:
-    try:
-        with open('proxies.txt', 'r') as file:
-            proxies = [proxy.replace('\n', '') for proxy in file.readlines()]
-            return random.choice(proxies)
-    except FileExistsError:
-        print('Create file `proxies.txt` in file `nft_bot`')
-        return ''
-
 # ToDo: refactoring
-def send_buy_requests(product_id: str = '', amount: str = '') -> None:
+def send_buy_requests(product_id: str, amount: str) -> None:
     threads = list()
-    product = Product(amount=50)
+    product = Product(product_id=product_id,amount=amount)
     start_sale_time = product.get_start_time()
 
     while True:
         current_time = datetime.today()
-        if start_sale_time >= (current_time + timedelta(seconds=12)):
+        if start_sale_time <= (current_time + timedelta(seconds=11.5)):
             count_requests = 0
             for _ in range(1, 1000):
                 count_requests += 1
-                new_product = Product(amount=50)
                 request = threading.Thread(
-                    target=new_product.buy_product,
-                    args=(get_random_proxy(),)
+                    target=product.buy_product,
+                    args=(PROXY,)
                 )
                 request.start()
                 threads.append(request)
                 time.sleep(0.13)
 
                 if count_requests == 100:
+                    print('The sale is over! Click CTRL+C or CTRL+Z')
                     time.sleep(10000)
-
-                if start_sale_time <= (current_time - timedelta(seconds=3)):
-                    print('The sale is over!')
-                    sys.exit(0)
 
             for thread in threads:
                 thread.join()
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from settings import headers
 
 headers_is_right(headers)
 
+product_id = str(input('Enter productId: '))
+amount = str(input('Enter nft`s price: '))
+
 print('Start waiting')
 
-send_buy_requests()
+send_buy_requests(product_id, amount)
