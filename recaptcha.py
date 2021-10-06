23d48b54a876b0a143e5db4ca648f8570cbc0815
Matvey Semenov <matveytri777@gmail.com>
2021-10-06 12:04:43 +0300

    update

--- recaptcha.py
+++ recaptcha.py
@@ -1,6 +1,8 @@
-from settings import BINANCE_SITEKEY, CAPTCHA_API_KEY
-
+import random
 from twocaptcha import TwoCaptcha
+
+from product import Product
+from settings import BINANCE_SITEKEY, CAPTCHA_API_KEY
 
 
 def resolve_captcha() -> str:
@@ -9,10 +11,13 @@ def resolve_captcha() -> str:
     # your own API KEY from https://2captcha.com/enterpage
     solver = TwoCaptcha(apiKey=CAPTCHA_API_KEY)
     url = 'https://www.binance.com/en/nft/goods/detail?productId='
+    product = Product()
+    product_ids = product.list_product_ids
+    product_id = random.choice(product_ids)
 
     result = solver.recaptcha(
         sitekey=BINANCE_SITEKEY,
-        url=url + '8532619' + '&isProduct=1',
+        url=url + str(product_id) + '&isProduct=1',
         version='v3',
         score='0.3'
     )
